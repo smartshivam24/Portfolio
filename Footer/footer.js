e2476bace39e1a6c34725b8fbbd4275b0d55a0a4
OM PRAKASH VISHWAKARMA <omprakashvishwakarma9696@gmail.com>
2026-08-06 11:23:16 +0530

    Refactor footer.js to fetch footer.html directly

--- Footer/footer.js
+++ Footer/footer.js
@@ -2,7 +2,7 @@ const path = window.location.pathname.includes("/Certificate/")
     ? "../Footer/footer.html"
     : "Footer/footer.html";
 
-fetch(path)
+fetch("footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
