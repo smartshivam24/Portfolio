0bf16f0f8d73e184b739a884cd8c8902a82213af
OM PRAKASH VISHWAKARMA <omprakashvishwakarma9696@gmail.com>
2026-08-06 11:25:00 +0530

    Fix fetch path for footer HTML file

--- Footer/footer.js
+++ Footer/footer.js
@@ -2,7 +2,7 @@ const path = window.location.pathname.includes("/Certificate/")
     ? "../Footer/footer.html"
     : "Footer/footer.html";
 
-fetch("footer.html")
+fetch("Footer/footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
