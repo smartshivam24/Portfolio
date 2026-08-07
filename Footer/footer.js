f91ed52fb1b3e1ba61aa571a230d5ac450c351e8
OM PRAKASH VISHWAKARMA <omprakashvishwakarma9696@gmail.com>
2026-08-08 04:35:59 +0530

    Delete Footer directory

--- Footer/footer.js
+++
@@ -1,9 +0,0 @@
-const path = window.location.pathname.includes("/Certificate/")
-    ? "../Footer/footer.html"
-    : "Footer/footer.html";
-
-fetch("/Footer/footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data;
-    });
